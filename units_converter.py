28ee104b0b580b7988b2d27567c5553476576ab4
Charlie Pilc <pilcwc@dukes.jmu.edu>
2022-03-01 19:56:17 -0500

    add precision specifier

--- units_converter.py
+++ units_converter.py
@@ -1,6 +1,7 @@
 """
-A units converter than can convert MPG to KPL
+A units converter than can convert MPG to KPL precisely to 4 decimal places
 """
+
+
 def mpg2kpl(mpg):
-    return 12.7543
-    
+    return round(mpg * 1.609344 * 0.2641720524, 4)
